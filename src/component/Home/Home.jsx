d0ddf13401000cb9a6fe5f89eac61529b98d0413
Abhinav Sinha <abhinavraj350@gmail.com>
2023-03-12 09:57:04 +0530

    design pending

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,41 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import "./home.css"
+import React, { useEffect, useState } from "react";
+import "./home.css";
 
-const Home = ({setFlag}) => {
-const [state,setState] = useState([]);
+const Home = ({ setFlag }) => {
+  const [state, setState] = useState([]);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-useEffect(()=>{
-fetchData()
-// console.log(state);
-},[])
-console.log(state);
-
-const fetchData = ()=>{
+  const fetchData = () => {
     try {
-        fetch('https://randomuser.me/api/?results=10').then((res)=>res.json()).then((data)=>{
-            console.log(data);
-            setState(data);
-        })
+      fetch("https://randomuser.me/api/?results=10")
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setState(data.results);
+        });
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-}
+  };
 
   return (
-    <div className='hmdiv'>
-        
-        <button onClick={()=>setFlag(false)} className="btnhm">Logout</button>
+    <div className="hmdiv">
+      <button onClick={() => setFlag(false)} className="btnhm">
+        Logout
+      </button>
 
-
-        <div className='main-div'>
-            <div className='snd-div'>
-
-            </div>
-
+      <div className="main-div">
+        <div className="snd-div">
+          {state.map((e, i) => {
+            return (
+              <div className="userDiv">
+                <div>
+                  <img src={e.picture.thumbnail} alt="" />
+                </div>
+                <div>
+                  <p>
+                    {" "}
+                    {e.name.title + " " + e.name.first + " " + e.name.last}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
